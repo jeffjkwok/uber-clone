4153494992dc0efc72a42c7576cbbb150a4568ae
Jeff Kwok <jeffjkwok@gmail.com>
2022-02-06 14:29:27 -0800

    Configure Map to have Pick up and Drop off markers while using MapViewDirections

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
 import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
+import { selectDestination, selectOrigin } from '../slices/navSlice';
+import MapViewDirections from 'react-native-maps-directions';
+import { API_KEY } from '@env';
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, bottom: 50, left: 50, right: 50 }
+    });
+  }, [origin, destination]);
+
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
+      mapType="mutedStandard"
       initialRegion={{
         latitude: origin.location.lat,
         longitude: origin.location.lng,
@@ -17,6 +31,16 @@ const Map = () => {
         longitudeDelta: 0.005
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={API_KEY}
+          strokeColor="black"
+          strockWidth={5}
+        />
+      )}
+
       {origin?.location && (
         <Marker
           coordinate={{
@@ -26,6 +50,18 @@ const Map = () => {
           title="Pick Up Location"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng
+          }}
+          title="Drop Off Location"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
